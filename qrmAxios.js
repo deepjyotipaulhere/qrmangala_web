159c8a57b085588da6e75455940234c2be6a4be2
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2022-01-07 20:45:18 +0530

    new

--- qrmAxios.js
+++ qrmAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const qrmAxios = axios.create({
   // baseURL: "http://localhost:8000/api",
-  baseURL: "http://qrmangala.azurewebsites.net/api",
+  baseURL: "https://qrmangala.azurewebsites.net/api",
   headers: {
     Authorization: `Bearer`,
   },
